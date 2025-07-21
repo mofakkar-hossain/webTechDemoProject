cac6d1c1b76b0373fbc3a2fc6385fd8a2ba9ef3e
Mofakkar Hossain <mofakkarhossain2001@gmail.com>
2025-07-22 03:14:44 +0600

    Add admin routes

---
+++ src/admin/admin.controller.ts
@@ -0,0 +1,38 @@
+import { Controller, Get, Post, Put, Delete, Param, Query, Body } from '@nestjs/common';
+import { CreateCounsellorDto } from './dtos/createCounsellor.dto'; 
+
+@Controller('admin')
+export class AdminController {
+    @Get('counsellor')
+    getAllCounsellors() {
+        return { message: 'Lists all the counsellors in the database' };
+    }
+
+    @Get('counsellor/search')
+    searchCounsellors(@Query('name') name: string) {
+        return { message: `Searching counsellors with named: ${name}` };
+    }
+
+    @Get('counsellor/:counsellorId')
+    getCounsellorById(@Param('counsellorId') counsellorId: string) {
+        return { message: `Retrieves a counsellor based on id: ${counsellorId}` };
+    }
+
+    @Post('counsellor')
+    createCounsellor(@Body() data: CreateCounsellorDto) {
+        return { data };
+    }
+
+    @Put('counsellor/:counsellorId')
+    updateCounsellor(
+        @Param('counsellorId') counsellorId: string,
+        @Body() data: CreateCounsellorDto
+    ) {
+        return { message: `Updates counsellor with id: ${counsellorId}`, data };
+    }
+
+    @Delete('counsellor/:counsellorId')
+    deleteCounsellor(@Param('counsellorId') counsellorId: string) {
+        return { message: `Deletes a counsellor based on id: ${counsellorId}` };
+    }
+}
---
+++ src/admin/dtos/createCounsellor.dto.ts
@@ -0,0 +1,4 @@
+export class CreateCounsellorDto{
+    name: string;
+    email:string;
+}
