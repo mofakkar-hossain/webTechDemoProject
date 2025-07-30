f4d8c5bf9e692ba711b618fb7c5c8e02bd4bd634
Md Ariful Islam <arifsofficials@gmail.com>
2025-07-30 10:18:11 +0600

    Pipes and file upload

--- src/seeker/seeker.service.ts
+++ src/seeker/seeker.service.ts
@@ -1,30 +1,34 @@
-import { Injectable } from "@nestjs/common";
+import { Injectable } from '@nestjs/common';
+import { createSeekerDto } from './dto/CreateSeeker.dto';
 
 @Injectable()
 export class SeekerService {
-     getScholarships() {
+  //CreateSeekerDto: string;
+  getScholarships() {
     return ['S1', 'S2', 'S2'];
-    }
-    searchScholarships(ScholarshipName:string) {
-        return { messege: 'Search for Schoalrships',ScholarshipName};
-    }
-    createApplication(data:any) {
-        return { messege: 'New application cretaed',data};
-    }
-    updateApplication(id: string, data: any) {
+  }
+  searchScholarships(ScholarshipName: string) {
+    return { messege: 'Search for Schoalrships', ScholarshipName };
+  }
+  createApplication(data: any) {
+    return { messege: 'New application cretaed', data };
+  }
+  updateApplication(id: string, data: any) {
     return { message: 'Application with id ${id} updated', data };
-    }
-    filterScholarships(country: string, degree: string) {
-        return {
-            message: 'Filtered scholarships',
-            filters: { country, degree }
-        };
-    }
-    deleteApplication(id: string) {
-        return { message: 'Deleted application', id };
-    }
+  }
+  filterScholarships(country: string, degree: string) {
+    return {
+      message: 'Filtered scholarships',
+      filters: { country, degree },
+    };
+  }
+  deleteApplication(id: string) {
+    return { message: 'Deleted application', id };
+  }
 
-
-
+  createSeeker(CreateSeekerDto: createSeekerDto): string {
+    const { name, email, password, phone } = CreateSeekerDto;
+    return 
+    'User created: ${name} (${email}) ${password} ${phone}';
+  }
 }
- 
