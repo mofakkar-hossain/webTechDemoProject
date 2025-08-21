ab605d1d479fdc1a4fa76729c85ead58609f9419
avishek shrabon <avishekshrabon3@gmail.com>
2025-08-21 12:09:36 +0600

    removed admin and seeker

--- src/seeker/seeker.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { SeekerController } from './seeker.controller';
-import { SeekerService } from './seeker.service';
-
-@Module({
-  controllers: [SeekerController],
-  providers: [SeekerService]
-})
-export class SeekerModule {}
